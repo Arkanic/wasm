bae9e31000f64e2008151f9053fe326202a94a65
Arkanic <silverfishasaurus@gmail.com>
2021-06-14 23:38:21 +0000

    sentence mode maker

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
 import * as wasm from "wasm-internals";
 
-wasm.greet();
+let sentenceInput = document.getElementById("sentence-input");
+let sentenceSubmit = document.getElementById("sentence-submit");
+let sentenceOutput = document.getElementById("sentence-output");
+
+/**
+ * Generates a html string of formatted object.
+ * 
+ * @param {Object} result the result
+ * 
+ * @returns {string} the html string
+ */
+function formatObj(result) {
+    let s = "";
+    for(let i in result) {
+        s += `<p>${i}: ${result[i]}</p>`;
+    }
+
+    return s;
+}
+
+sentenceSubmit.addEventListener("click", (e) => {
+    let sentence = sentenceInput.value;
+    let result = wasm.sentence_mode(sentence);
+
+    sentenceOutput.innerHTML = formatObj(result);
+});
--- src/rs/src/lib.rs
+++ src/rs/src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use std::collections::HashMap;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
@@ -12,6 +13,12 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("HHHHH");
+pub fn sentence_mode(s:&str) -> JsValue {
+    let mut map = HashMap::new();
+    for word in s.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    return JsValue::from_serde(&map).unwrap();
 }
